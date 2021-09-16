6657d3c5e54e3596023631679d6963d67cd8a268
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-09-16 12:39:42 +0700

    bosung

---
+++ demo.js
@@ -0,0 +1,12 @@
+// let number = parseInt(prompt("Nhập số: "))
+// let check = number > 0 ? "Positive" : number < 0 ? "Negative" : "0"
+// switch (check) {
+//     case "Positive":
+//         alert(1)
+//         break
+//     case  "Negative":
+//         alert(-1)
+//         break
+//     default:
+//         alert(0)
+// }
